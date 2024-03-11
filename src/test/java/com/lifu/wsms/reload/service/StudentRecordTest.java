ee09a56c3b3167b805d6495023cc46aa15ad5e0c
Anthony Ohiero Lifu <tonylifu@outlook.com>
2024-03-11 19:01:18 +0000

    adds bean config and test class stub for student

---
+++ src/test/java/com/lifu/wsms/reload/service/StudentRecordTest.java
@@ -0,0 +1,25 @@
+package com.lifu.wsms.reload.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class StudentRecordTest {
+
+    @Test
+    void createStudent() {
+    }
+
+    @Test
+    void findStudent() {
+    }
+
+    @Test
+    void updateStudent() {
+    }
+
+    @Test
+    void deleteStudent() {
+    }
+}
