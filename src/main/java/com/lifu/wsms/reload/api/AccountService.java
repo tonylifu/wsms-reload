079ec0a6d17b0319c18b08d5e60f51e8296fa836
Anthony Ohiero Lifu <tonylifu@outlook.com>
2024-03-15 16:42:20 +0000

    removes unused imports

--- src/main/java/com/lifu/wsms/reload/api/AccountService.java
+++ src/main/java/com/lifu/wsms/reload/api/AccountService.java
@@ -1,4 +1,10 @@
 package com.lifu.wsms.reload.api;
 
+import com.lifu.wsms.reload.dto.response.ApiResponse;
+
+import java.math.BigDecimal;
+
 public interface AccountService {
+    ApiResponse postTransaction(String destinationAccount, BigDecimal amount);
+    ApiResponse fundTransfer(String sourceAccount, String destinationAccount, BigDecimal amount);
 }
--- src/main/java/com/lifu/wsms/reload/dto/response/SuccessResponse.java
+++ src/main/java/com/lifu/wsms/reload/dto/response/SuccessResponse.java
@@ -4,11 +4,6 @@ import com.fasterxml.jackson.databind.JsonNode;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.http.HttpStatus;
-
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
 
 @Getter
 @Setter
