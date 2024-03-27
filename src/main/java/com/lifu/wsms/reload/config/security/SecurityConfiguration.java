f293c7df3e7ef16664fc9ed01b4bc022089d965f
Anthony Ohiero Lifu <tonylifu@outlook.com>
2024-03-27 21:48:08 +0000

    removes EnableWebSecurity annotation

--- src/main/java/com/lifu/wsms/reload/config/security/SecurityConfiguration.java
+++ src/main/java/com/lifu/wsms/reload/config/security/SecurityConfiguration.java
@@ -1,20 +1,15 @@
 package com.lifu.wsms.reload.config.security;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
-import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
-
 @Configuration
 @RequiredArgsConstructor
-@EnableWebSecurity
 public class SecurityConfiguration {
     private final JwtAuthConverter jwtAuthConverter;
 
