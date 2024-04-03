8247e0a70fe479b19f9cb43fe48eb8d16e7165ea
Anthony Ohiero Lifu <tonylifu@outlook.com>
2024-04-03 02:35:25 +0100

    removes space in import

--- src/main/java/com/lifu/wsms/reload/entity/user/User.java
+++ src/main/java/com/lifu/wsms/reload/entity/user/User.java
@@ -8,7 +8,6 @@ import jakarta.persistence.*;
 import lombok.Data;
 import org.hibernate.annotations.JdbcTypeCode;
 import org.hibernate.type.SqlTypes;
-
 import java.io.Serializable;
 import java.util.HashSet;
 import java.util.List;
