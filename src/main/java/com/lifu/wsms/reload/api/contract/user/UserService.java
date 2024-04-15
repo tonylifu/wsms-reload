bb9254901eef6cc29e82acd5d4e84dd58f947148
Anthony Ohiero Lifu <tonylifu@outlook.com>
2024-04-15 21:47:03 +0100

    refactors userservice

--- src/main/java/com/lifu/wsms/reload/api/contract/user/UserService.java
+++ src/main/java/com/lifu/wsms/reload/api/contract/user/UserService.java
@@ -14,7 +14,7 @@ import java.util.Set;
 /**
  * Interface for user-related operations.
  */
-public interface UserService {
+public interface UserService extends RoleService {
 
     /**
      * Creates a new user.
@@ -76,30 +76,5 @@ public interface UserService {
      */
     ApiResponse updateStatus(String username, UserStatus status);
 
-    /**
-     * Adds roles to a user.
-     *
-     * @param username The username of the user to add roles to.
-     * @param roles    The set of roles to add.
-     * @return An {@link ApiResponse} indicating the result of the operation.
-     */
-    ApiResponse addRoles(String username, Set<UserRole> roles);
-
-    /**
-     * Removes all roles from a user.
-     *
-     * @param username The username of the user to remove roles from.
-     * @return An {@link ApiResponse} indicating the result of the operation.
-     */
-    ApiResponse removeAllRoles(String username);
-
-    /**
-     * Removes a specific role from a user.
-     *
-     * @param username The username of the user from which the role will be removed.
-     * @param role     The role to be removed from the user.
-     * @return An {@link ApiResponse} indicating the outcome of the operation.
-     */
-    ApiResponse removeRole(String username, UserRole role);
 }
 
