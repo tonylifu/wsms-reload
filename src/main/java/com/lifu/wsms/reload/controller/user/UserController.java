623e43dbf6c432ad5f3f7608054f63931727f0e9
Anthony Ohiero Lifu <tonylifu@outlook.com>
2024-04-25 19:37:14 +0100

    implements sets password tests

--- src/main/java/com/lifu/wsms/reload/controller/user/UserController.java
+++ src/main/java/com/lifu/wsms/reload/controller/user/UserController.java
@@ -2,6 +2,8 @@ package com.lifu.wsms.reload.controller.user;
 
 import com.lifu.wsms.reload.api.contract.user.UserService;
 import com.lifu.wsms.reload.dto.request.user.CreateUserRequest;
+import com.lifu.wsms.reload.dto.request.user.PasswordSetRequest;
+import com.lifu.wsms.reload.dto.request.user.UpdateUserRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,9 +15,10 @@ public class UserController {
 
     public static final String USER_PATH = "/api/v1/users";
     public static final String USER_PATH_USERNAME = USER_PATH + "/{username}";
+    public static final String USER_PATH_PASSWORD_SET = USER_PATH_USERNAME + "/password";
 
     @PostMapping(USER_PATH)
-    public ResponseEntity<?> createStudent(@RequestBody final CreateUserRequest createUserRequest) {
+    public ResponseEntity<?> createUser(@RequestBody final CreateUserRequest createUserRequest) {
         return userService.createUser(createUserRequest)
                 .fold(
                         failureResponse -> ResponseEntity
@@ -46,5 +49,31 @@ public class UserController {
                                         successResponse.getBody().get("username").asText())
                                 .body(successResponse)
                 );
+    }
+
+    @PutMapping(USER_PATH)
+    public ResponseEntity<?> updateUser(@RequestBody final UpdateUserRequest updateUserRequest) {
+        return userService.updateUser(updateUserRequest)
+                .fold(
+                        failureResponse -> ResponseEntity
+                                .status(failureResponse.getApiResponse().getHttpStatusCode())
+                                .headers(failureResponse.getApiResponse().getHttpHeaders())
+                                .body(failureResponse),
+                        successResponse -> ResponseEntity
+                                .status(successResponse.getApiResponse().getHttpStatusCode())
+                                .headers(successResponse.getApiResponse().getHttpHeaders())
+                                .header("location", USER_PATH + "/" +
+                                        successResponse.getBody().get("username").asText())
+                                .body(successResponse)
+                );
+    }
+
+    @PutMapping(USER_PATH_PASSWORD_SET)
+    public ResponseEntity<?> setPassword(@PathVariable("username") String username, @RequestBody final PasswordSetRequest passwordSetRequest) {
+        var response = userService.setPassword(username, passwordSetRequest.getPassword().toCharArray());
+        return ResponseEntity
+                .status(response.getHttpStatusCode())
+                .header("location", USER_PATH + "/" + username)
+                .body(response);
     }
 }
--- src/test/java/com/lifu/wsms/reload/controller/user/UserControllerTest.java
+++ src/test/java/com/lifu/wsms/reload/controller/user/UserControllerTest.java
@@ -2,6 +2,8 @@ package com.lifu.wsms.reload.controller.user;
 
 import com.jayway.jsonpath.DocumentContext;
 import com.jayway.jsonpath.JsonPath;
+import com.lifu.wsms.reload.dto.request.user.PasswordSetRequest;
+import com.lifu.wsms.reload.enums.UserRole;
 import net.minidev.json.JSONArray;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,9 +13,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
+
+import java.util.HashSet;
+import java.util.Set;
+
 import static com.lifu.wsms.reload.controller.user.UserController.USER_PATH;
 import static com.lifu.wsms.reload.util.UserTestUtil.getCreateUserDTO;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static com.lifu.wsms.reload.util.UserTestUtil.getUpdateUserDTO;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
@@ -24,7 +31,7 @@ public class UserControllerTest {
 
     @Test
     void shouldCreateAndReadUser() {
-        //Given
+        //Given - CREATE/READ
         var createUserRequest = getCreateUserDTO();
 
         //When
@@ -48,5 +55,46 @@ public class UserControllerTest {
         assertEquals(createUserRequest.getUsername().toUpperCase(), username);
         assertEquals(createUserRequest.getEmail(), email);
         assertEquals(createUserRequest.getRoles().size(), roles.size());
+
+        //Given - UPDATE
+        var updateUserRequest = getUpdateUserDTO();
+        updateUserRequest.setUsername(username);
+        assertEquals(createUserRequest.getUsername().toUpperCase(), updateUserRequest.getUsername());
+        assertNotEquals(createUserRequest.getMobile(), updateUserRequest.getMobile());
+        assertNotEquals(createUserRequest.getEmail(), updateUserRequest.getEmail());
+
+        //When
+        restTemplate.put(USER_PATH, updateUserRequest);
+        ResponseEntity<String> getUserResponseEntityAfterUpdate = restTemplate.getForEntity(location, String.class);
+
+        //Then
+        DocumentContext documentContextAfterUpdate = JsonPath.parse(getUserResponseEntityAfterUpdate.getBody());
+        String usernameAterUpdate = documentContextAfterUpdate.read("$.body.username");
+        String emailAfterUpdate = documentContextAfterUpdate.read("$.body.email");
+        JSONArray rolesAfterUpdate = documentContextAfterUpdate.read("$.body.roles");
+
+        Set<UserRole> updatedRoles = new HashSet<>();
+        updatedRoles.addAll(createUserRequest.getRoles());
+        updatedRoles.addAll(updateUserRequest.getRoles() != null ? updateUserRequest.getRoles() : Set.of());
+
+        assertEquals(updateUserRequest.getUsername().toUpperCase(), usernameAterUpdate);
+        assertEquals(updateUserRequest.getEmail(), emailAfterUpdate);
+        assertEquals(updatedRoles.size(), rolesAfterUpdate.size());
+
+        //Given - Set Password
+        String password = "Password@1234";
+        PasswordSetRequest passwordSetRequest = new PasswordSetRequest();
+        passwordSetRequest.setPassword(password);
+
+        //And
+        boolean isPasswordSet = documentContextAfterUpdate.read("$.body.passwordSet");
+        assertFalse(isPasswordSet);
+
+        //When
+        restTemplate.put(location + "/password", passwordSetRequest);
+        ResponseEntity<String> getUserResponseEntityAfterPasswordSet = restTemplate.getForEntity(location, String.class);
+        DocumentContext documentContextAfterPasswordSet = JsonPath.parse(getUserResponseEntityAfterPasswordSet.getBody());
+        boolean passwordSet = documentContextAfterPasswordSet.read("$.body.passwordSet");
+        assertTrue(passwordSet);
     }
 }
